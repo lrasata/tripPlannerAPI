af3159c35f0ded7013bc20bb9c30fa87d1725f56
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-06-26 10:30:41 +0200

    fix: configure db for test for github actions

--- src/test/java/com/lrasata/tripPlannerAPI/TripPlannerAPIApplicationTests.java
+++ src/test/java/com/lrasata/tripPlannerAPI/TripPlannerAPIApplicationTests.java
@@ -1,6 +1,8 @@
 package com.lrasata.tripPlannerAPI;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TripPlannerAPIApplicationTests {}
